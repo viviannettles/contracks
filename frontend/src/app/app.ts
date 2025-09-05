ca3aa7e5114292f361a956c27db1953deeb6dc28
viviannettles <vivnett@gmail.com>
2025-09-04 22:49:46 -0500

    9/4/2025: beginnings of app! :D

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,5 +1,5 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +9,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('frontend');
+
+
 }
